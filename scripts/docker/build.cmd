a126c1f9a8a0afeed4d89fbabd27d960d8ee7727
Erich Wang <eriwan@microsoft.com>
2018-04-13 18:27:37 +0800

    update script

--- scripts/docker/build.cmd
+++ scripts/docker/build.cmd
@@ -1,7 +1,7 @@
 @ECHO off & setlocal enableextensions enabledelayedexpansion
 
 :: Note: use lowercase names for the Docker images
-SET DOCKER_IMAGE=azureiotpcs/pcs-storage-adapter-dotnet
+SET DOCKER_IMAGE=vschinaiot/pcs-storage-adapter
 :: "testing" is the latest dev build, usually matching the code in the "master" branch
 SET DOCKER_TAG=%DOCKER_IMAGE%:testing
 
--- scripts/docker/run.cmd
+++ scripts/docker/run.cmd
@@ -1,7 +1,7 @@
 @ECHO off & setlocal enableextensions enabledelayedexpansion
 
 :: Note: use lowercase names for the Docker images
-SET DOCKER_IMAGE="azureiotpcs/pcs-storage-adapter-dotnet"
+SET DOCKER_IMAGE="vschinaiot/pcs-storage-adapter"
 
 :: strlen("\scripts\docker\") => 16
 SET APP_HOME=%~dp0
